c2322446e40c88d056b6401beac1385b210b3412
Vladimir Sedlacek <vlada.sedlacek@mail.muni.cz>
2023-04-12 18:46:53 -0400

    Add a forgotten import.

--- test.lean
+++ test.lean
@@ -1,3 +1,6 @@
+import tactics
+
+
 lemma test_perm {a b c d e f u v w x y z: point} (h: area u v w + area x y z = 1)
   : (area a c c = area c c a) ∧ (area a b c + area e d f = area a c b + area d f e) ∧ (area a b c + area c b a = area a b c * 2) ∧ (area b a c + area c b a = area a b c * 2) ∧ (area b a c + area a b c = area a b c * 2) := by
   have := h
