aba3b4d585eb04c229c97afcdcff6a75334effa9
Vladimir Sedlacek <vlada.sedlacek@mail.muni.cz>
2023-04-15 17:56:05 -0400

    Fix the import.

--- lakefile.lean
+++ lakefile.lean
@@ -1,14 +1,10 @@
 import Lake
 open Lake DSL
 
-package «synthetic_euclid_4» {
-  -- add any package configuration options here
-}
-
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+package synthetic_euclid_4
+
 @[default_target]
-lean_lib «SyntheticEuclid4» {
-  -- add any library configuration options here
-}
+lean_lib SyntheticEuclid4
