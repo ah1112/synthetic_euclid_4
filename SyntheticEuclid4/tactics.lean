c6ee281b3a9ca509ccb18e9b05f6444814ee09f0
Vladimir Sedlacek <vlada.sedlacek@mail.muni.cz>
2023-04-25 08:46:10 -0400

    Clean up.

--- SyntheticEuclid4/tactics.lean
+++ SyntheticEuclid4/tactics.lean
@@ -13,52 +13,45 @@ lemma ar213 {a b c : point} : area a b c = area b a c
 lemma ar321 {a b c : point} : area a b c = area c b a
   := by rw [(area_invariant a b c).2]; rw [(area_invariant c b a).1]
 
-/-- ## Tactic perm
-A custom experimental tactic for lexicographically permuting the order of operands, currently only working for triangle areas.
-
-Usage:
-- `perm` lexicographically permutes the goal
-- `perm at h` lexicographically permutes hypothesis h
-- `perm [213]` permutes the first two arguments of the goal
- -/
-syntax "perm" "["? ("←"?num),* "]"? ("at" ident)? : tactic
-
-macro_rules
--- explicit versions
-  | `(tactic| perm [132]) => `(tactic| rw [@ar132 _ _ _])
-  | `(tactic| perm [312]) => `(tactic| rw [@ar312 _ _ _])
-  | `(tactic| perm [231]) => `(tactic| rw [@ar231 _ _ _])
-  | `(tactic| perm [213]) => `(tactic| rw [@ar213 _ _ _])
-  | `(tactic| perm [321]) => `(tactic| rw [@ar321 _ _ _])
-
 namespace Lean.Elab.Tactic
 
 def lte (a : @& Expr) (b : @& Expr) : Bool :=
   Lean.Expr.lt a b || Lean.Expr.equal a b
 
-syntax (name := perm_nf) "perm_nf" : conv
+/-- ## Conv tactic `perm_nf`
+A conv tactic for permuting the variables in a single geometric primitive. A building block for the `perm` tactic.
+ -/
+syntax "perm_nf" : conv
 elab_rules : conv 
   |`(conv| perm_nf) => do
-  withMainContext do
-    let tgt ← instantiateMVars (← Conv.getLhs)
-    let arg1 := Lean.Expr.getArg! tgt 1
-    let arg2 := Lean.Expr.getArg! tgt 2
-    let arg3 := Lean.Expr.getArg! tgt 3
-    if lte arg1 arg2 && lte arg2 arg3 then
-      evalTactic (← `(tactic| try rfl )) -- abc
-    else if lte arg1 arg3 && lte arg3 arg2 then
-      evalTactic (← `(tactic| perm [132] )) -- acb
-    else if lte arg2 arg1 && lte arg1 arg3 then
-      evalTactic (← `(tactic| perm [213] )) -- bac
-    else if lte arg3 arg1 && lte arg1 arg2 then
-      evalTactic (← `(tactic| perm [312] )) -- bca
-    else if lte arg2 arg3 && lte arg3 arg1 then
-      evalTactic (← `(tactic| perm [231] )) -- cab
-    else if lte arg3 arg2 && lte arg2 arg1 then
-      evalTactic (← `(tactic| perm [321] )) -- cba
+    withMainContext do
+      let tgt ← instantiateMVars (← Conv.getLhs)
+      let arg1 := Lean.Expr.getArg! tgt 1
+      let arg2 := Lean.Expr.getArg! tgt 2
+      let arg3 := Lean.Expr.getArg! tgt 3
+      if lte arg1 arg2 && lte arg2 arg3 then
+        evalTactic (← `(tactic| try rfl )) -- abc
+      else if lte arg1 arg3 && lte arg3 arg2 then
+        evalTactic (← `(tactic| rw [@ar132 _ _ _] )) -- acb
+      else if lte arg2 arg1 && lte arg1 arg3 then
+        evalTactic (← `(tactic| rw [@ar213 _ _ _] )) -- bac
+      else if lte arg3 arg1 && lte arg1 arg2 then
+        evalTactic (← `(tactic| rw [@ar312 _ _ _] )) -- bca
+      else if lte arg2 arg3 && lte arg3 arg1 then
+        evalTactic (← `(tactic| rw [@ar231 _ _ _] )) -- cab
+      else if lte arg3 arg2 && lte arg2 arg1 then
+        evalTactic (← `(tactic| rw [@ar321 _ _ _] )) -- cba
 
+/-- ## Tactic perm
+A custom experimental tactic for permuting the variables in geometric primitives. The ordering is the one in which the variables are introduced, so it is not necessarily lexigraphic in general. Currently only working for triangle areas.
+
+Usage:
+- `perm` permutes the variables in the goal
+- `perm at h` permutes the variables in hypothesis h
+- `perm at *` TODO: permutes the variables in the goal and all hypotheses
+ -/
+syntax "perm" ("at" ident)? : tactic
 macro_rules
--- implicit versions
   | `(tactic| perm) => `(tactic|
   {
     conv =>
@@ -84,8 +77,6 @@ macro_rules
       args
       all_goals perm_nf
   })
-
--- TODO: ← apply to all goals and hypotheses by default
 
 syntax "perm'" ("at" ident)? : tactic
 macro_rules
